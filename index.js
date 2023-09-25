73f60ec9548156f25a5b1536a0b3e5e1d2892074
waliullah55 <waliullah9099@gmail.com>
2023-09-25 12:04:39 +0600

    specipic id select

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const port = process.env.PORT || 5000;
 const category = require('./data/category.json')
 const banner = require('./data/banner.json')
 const products = require('./data/product.json')
+const chefs = require('./data/chefs.json')
 
 
 app.use(cors())
 app.use(express.json())
 
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+});
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedId = chefs.find(n => n.id == id);
+    res.send(selectedId);
+})
 app.get('/products', (req, res) => {
     res.send(products)
 });
